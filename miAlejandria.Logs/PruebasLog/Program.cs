507430a46868326ac686ab30d3f6c179d1b6bfa9
Arkadoel Rugnik <arkadoel@hotmail.com>
2014-04-03 23:25:16 +0200

    Poniendo proyecto Visual studio

---
+++ miAlejandria.Logs/PruebasLog/Program.cs
@@ -0,0 +1,18 @@
+﻿using FeedBackManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logs.initLog();
+            Logs.WriteText("titulo", "prueba");
+        }
+    }
+}
